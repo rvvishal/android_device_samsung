9bd720ab45be57d72513bdfe1b9a73d6385eb19d
Gabriel260 <adeilmazk@gmail.com>
2021-03-06 05:56:40 -0300

    exynos7885: build arm32 system
    Signed-off-by: Gabriel260 <adeilmazk@gmail.com>

--- a10/full_a10.mk
+++ a10/full_a10.mk
@@ -3,7 +3,6 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
