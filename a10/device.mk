f3d6c6d0053d5c69e896323111902e271f8eb620
chatur27 <jasonbright2709@gmail.com>
2021-04-11 20:04:22 +0000

    a10: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- a10/device.mk
+++ a10/device.mk
@@ -2,3 +2,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal7885-common/universal7885-common.mk)
+
+# Usb hal
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.a10
