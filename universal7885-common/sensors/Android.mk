348255b42a9fc1084240633b0af607e2653e3862
chatur27 <jasonbright2709@gmail.com>
2021-04-14 17:46:39 +0000

    a10: Set up arm DotOs

--- universal7885-common/sensors/Android.mk
+++ universal7885-common/sensors/Android.mk
@@ -10,7 +10,6 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_VENDOR_MODULE := true
 
 LOCAL_MODULE_PATH_32 := $(TARGET_OUT_PRODUCT)/vendor_overlay/29/lib
-LOCAL_MODULE_PATH_64 := $(TARGET_OUT_PRODUCT)/vendor_overlay/29/lib64
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SRC_FILES := \
@@ -30,8 +29,5 @@ LOCAL_STATIC_LIBRARIES := \
     multihal
 
 LOCAL_POST_INSTALL_CMD := mkdir -p $(LOCAL_MODULE_PATH_32)/hw
-LOCAL_POST_INSTALL_CMD += ; mkdir -p $(LOCAL_MODULE_PATH_64)/hw
 LOCAL_POST_INSTALL_CMD += ; ln -sf $(LOCAL_MODULE).so $(LOCAL_MODULE_PATH_32)/hw/android.hardware.sensors@1.0-impl.so
-LOCAL_POST_INSTALL_CMD += ; ln -sf $(LOCAL_MODULE).so $(LOCAL_MODULE_PATH_64)/hw/android.hardware.sensors@1.0-impl.so
-
 include $(BUILD_SHARED_LIBRARY)
