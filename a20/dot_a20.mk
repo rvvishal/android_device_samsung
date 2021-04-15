24fc0858e7673c22a16ccc2a613c4ffc2db65a97
chatur27 <jasonbright2709@gmail.com>
2021-04-15 12:16:15 +0000

    Fix build error

--- a20/dot_a20.mk
+++ a20/dot_a20.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/a20/full_a20.mk)
 
 # Inherit some common DotOs stuff.
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
